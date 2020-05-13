69423c87be06b054489f4e81005e0e0cba153d1c
djaciel <dj_roses@hotmail.com>
2020-05-13 01:59:31 -0500

    initial commit

---
+++ Data/crudcoreContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using crudcore.Models;
+
+namespace crudcore.Data
+{
+    public class crudcoreContext : DbContext
+    {
+        public crudcoreContext (DbContextOptions<crudcoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<crudcore.Models.Employee> Employee { get; set; }
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace crudcore.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+    }
+}
